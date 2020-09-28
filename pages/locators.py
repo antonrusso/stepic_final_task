79eed08d6a31e89dfe7f144502a05b7c942d39bc
AntonRusso <aantonrusso@gmail.com>
2020-09-28 23:08:01 +0300

    created LoginPageLocators, added reg/login selectors

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    REG_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_CONFIRM_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+
+    LOGIN_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD_FIELD = (By.CSS_SELECTOR,"#id_login-password")
